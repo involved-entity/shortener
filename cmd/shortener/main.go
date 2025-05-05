cc48c622e653de870b5f60cdce601c6ef93c15d1
Something Bad <nglykhov@gmail.com>
2025-05-05 17:35:55 +0300

    Add pgadmin, models, migrations

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"shortener/internal"
+	"shortener/internal/database"
 )
 
 const (
@@ -16,7 +17,8 @@ func main() {
 	log := setupLogger(config.Env)
 	log.Info("Starting shortener service", "env", config.Env)
 	log.Info("HTTP server address", "address", config.HTTPServer.Address)
-	connection, error := internal.NewDatabase(config.DSN)
+	error := database.Init(config.DSN)
+	connection := database.GetDB()
 	log.Info("Database connection", "connection", connection, "error", error)
 }
 
