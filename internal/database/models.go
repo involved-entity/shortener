83dbb859c876ea25f8d1080d4d9a8b8fdb9ddb26
Something Bad <nglykhov@gmail.com>
2025-05-06 17:51:09 +0300

    Add User model

--- internal/database/models.go
+++ internal/database/models.go
@@ -19,3 +19,12 @@ type Click struct {
 	IPAddress string    `gorm:"not null"`
 	ClickedAt time.Time `gorm:"autoCreateTime"`
 }
+
+type User struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	JoinedAt   time.Time `gorm:"autoCreateTime" json:"joinedAt"`
+	Username   string    `gorm:"uniqueIndex;not null" json:"username"`
+	Password   string    `gorm:"not null" json:"-"`
+	Email      string    `gorm:"uniqueIndex;not null" json:"email"`
+	IsVerified bool      `gorm:"default:false" json:"-"`
+}
