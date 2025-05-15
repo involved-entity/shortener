802f9d3f583bb97a2e20deb17caf2b2029040e91
Something Bad <nglykhov@gmail.com>
2025-05-16 00:20:36 +0300

    Refactor tests with new base class and services

--- internal/api/urls/views_test.go
+++ internal/api/urls/views_test.go
@@ -1,118 +1,97 @@
 package urls
 
 import (
-	"log"
+	"bytes"
 	"net/http"
-	"net/http/httptest"
-	"os"
 	"shortener/internal/api/users"
-	"shortener/internal/database"
-	testutils "shortener/test_utils"
-	"strings"
+	testUtils "shortener/test_utils"
 	"testing"
-
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
 )
 
 var JWT string
 
 func TestMain(m *testing.M) {
-	rClient := testutils.InitTest()
+	rClient := testUtils.InitTest()
 
-	JWT = testutils.LoginHelper(users.Register, users.Login)
+	JWT = testUtils.LoginHelper(users.Register, users.Login)
 
 	exitCode := m.Run()
 
-	rClient.Close()
-	conn := database.GetDB()
-	tables := []string{
-		"users",
-		"urls",
-		"clicks",
-	}
-
-	query := "DROP TABLE IF EXISTS " + strings.Join(tables, ", ") + " CASCADE;"
-	err := conn.Exec(query).Error
-	if err != nil {
-		log.Fatalf("Ошибка при очистке базы данных: %v", err)
-	}
-
-	os.Exit(exitCode)
+	testUtils.ExitTest(rClient, exitCode)
 }
 
 func TestSaveURL(t *testing.T) {
-	e := echo.New()
-	urlDTO := `{"originalURL":"https://example.com","shortCode":"sh"}`
-	req := httptest.NewRequest(http.MethodPost, "/api/urls", strings.NewReader(urlDTO))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	basicTest := testUtils.BasicTest{
+		Method:         http.MethodPost,
+		Url:            "/api/urls",
+		Data:           bytes.NewBuffer([]byte(`{"originalURL":"https://example.com","shortCode":"sh"}`)),
+		ExpectedStatus: http.StatusOK,
+		Handler:        SaveURL,
+		T:              t,
 
-	parsedToken := testutils.GetJWTForTest(t, JWT)
-	c.Set("user", parsedToken)
+		JWT: JWT,
+	}
 
-	err := SaveURL(c)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, rec.Code)
+	basicTest.Execute()
 }
 
 func TestGetURL(t *testing.T) {
-	e := echo.New()
-	e.GET("/_/:shortCode", GetURL)
-	req := httptest.NewRequest(http.MethodGet, "/_/sh", nil)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	basicTest := testUtils.BasicTest{
+		Method:         http.MethodGet,
+		Url:            "/_/sh",
+		Data:           &bytes.Buffer{},
+		ExpectedStatus: http.StatusPermanentRedirect,
+		Handler:        GetURL,
+		T:              t,
 
-	parsedToken := testutils.GetJWTForTest(t, JWT)
-	c.Set("user", parsedToken)
+		ServeHTTPMode: true,
+		HandlerUrl:    "/_/:shortCode",
+	}
 
-	e.ServeHTTP(rec, req)
-	assert.Equal(t, http.StatusPermanentRedirect, rec.Code)
+	basicTest.Execute()
 }
 
 func TestDeleteURL(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodDelete, "/api/urls/sh", nil)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	basicTest := testUtils.BasicTest{
+		Method:         http.MethodDelete,
+		Url:            "/api/urls/sh",
+		Data:           &bytes.Buffer{},
+		ExpectedStatus: http.StatusNoContent,
+		Handler:        DeleteURL,
+		T:              t,
 
-	parsedToken := testutils.GetJWTForTest(t, JWT)
-	c.Set("user", parsedToken)
+		JWT: JWT,
+	}
 
-	err := DeleteURL(c)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusNoContent, rec.Code)
+	basicTest.Execute()
 }
 
 func TestGetMyURLs(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/api/urls", nil)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	basicTest := testUtils.BasicTest{
+		Method:         http.MethodGet,
+		Url:            "/api/urls",
+		Data:           &bytes.Buffer{},
+		ExpectedStatus: http.StatusOK,
+		Handler:        GetMyURLs,
+		T:              t,
 
-	parsedToken := testutils.GetJWTForTest(t, JWT)
-	c.Set("user", parsedToken)
+		JWT: JWT,
+	}
 
-	err := GetMyURLs(c)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, rec.Code)
+	basicTest.Execute()
 }
 
 func TestGetURLClicks(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/api/clicks/sh", nil)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	basicTest := testUtils.BasicTest{
+		Method:         http.MethodGet,
+		Url:            "/api/clicks/sh",
+		Data:           &bytes.Buffer{},
+		ExpectedStatus: http.StatusOK,
+		Handler:        GetURLClicks,
+		T:              t,
 
-	parsedToken := testutils.GetJWTForTest(t, JWT)
-	c.Set("user", parsedToken)
+		JWT: JWT,
+	}
 
-	err := GetURLClicks(c)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, rec.Code)
+	basicTest.Execute()
 }
--- test_utils/services.go
+++ test_utils/services.go
@@ -2,19 +2,41 @@ package testutils
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httptest"
+	"os"
 	conf "shortener/internal/config"
 	"shortener/internal/database"
+	r "shortener/internal/redis"
+	"strings"
 	"testing"
 
+	"github.com/go-redis/redis/v9"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
+
+func ExitTest(rClient *redis.Client, exitCode int) {
+	rClient.Close()
+	conn := database.GetDB()
+	tables := []string{
+		"users",
+		"urls",
+		"clicks",
+	}
+
+	query := "DROP TABLE IF EXISTS " + strings.Join(tables, ", ") + " CASCADE;"
+	err := conn.Exec(query).Error
+	if err != nil {
+		log.Fatalf("Ошибка при очистке базы данных: %v", err)
+	}
+
+	os.Exit(exitCode)
+}
 
 func GetJWTForTest(t *testing.T, JWT string) *jwt.Token {
 	config := conf.GetConfig()
@@ -25,16 +47,22 @@ func GetJWTForTest(t *testing.T, JWT string) *jwt.Token {
 	return parsedToken
 }
 
-func LoginHelper(register, login func(c echo.Context) error) string {
-	e := echo.New()
-	e.POST("/api/register", register)
-	e.POST("/api/login", login)
+func GetRedisVarForTestUser(redisName string) string {
+	redisClient := r.GetClient()
+	val, err := redisClient.Get(context.Background(), redisName+":1").Result()
+	if err != nil {
+		return ""
+	}
 
-	if err := registerUser(e); err != nil {
+	return val
+}
+
+func LoginHelper(register, login func(c echo.Context) error) string {
+	if err := registerUser(register); err != nil {
 		log.Fatalf("Failed to register user: %v", err)
 	}
 
-	token, err := loginUser(e, "testu", "12345678")
+	token, err := loginUser(login, "testu", "12345678")
 	if err != nil {
 		log.Fatalf("Failed to login user: %v", err)
 	}
@@ -42,13 +70,18 @@ func LoginHelper(register, login func(c echo.Context) error) string {
 	return token
 }
 
-func registerUser(e *echo.Echo) error {
-	reqBody := `{"username": "testu", "email": "test@example.com", "password": "12345678"}`
-	req := httptest.NewRequest(http.MethodPost, "/api/register", bytes.NewBuffer([]byte(reqBody)))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
+func registerUser(register func(c echo.Context) error) error {
+	basicTest := BasicTest{
+		Method:         http.MethodPost,
+		Url:            "/api/register",
+		Data:           bytes.NewBuffer([]byte(`{"username": "testu", "email": "test@example.com", "password": "12345678"}`)),
+		ExpectedStatus: http.StatusOK,
+		Handler:        register,
 
-	e.ServeHTTP(rec, req)
+		NotTestEnv: true,
+	}
+
+	basicTest.Execute()
 
 	db := database.GetDB()
 	if err := db.Model(&database.User{}).Where("username = ?", "testu").Update("is_verified", true).Error; err != nil {
@@ -58,13 +91,19 @@ func registerUser(e *echo.Echo) error {
 	return nil
 }
 
-func loginUser(e *echo.Echo, username, password string) (string, error) {
-	reqBody := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password)
-	req := httptest.NewRequest(http.MethodPost, "/api/login", bytes.NewBuffer([]byte(reqBody)))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
+func loginUser(login func(c echo.Context) error, username, password string) (string, error) {
+	basicTest := BasicTest{
+		Method:         http.MethodPost,
+		Url:            "/api/login",
+		Data:           bytes.NewBuffer([]byte(fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password))),
+		ExpectedStatus: http.StatusOK,
+		Handler:        login,
 
-	e.ServeHTTP(rec, req)
+		ServeHTTPMode: true,
+		NotTestEnv:    true,
+	}
+
+	rec := basicTest.Execute()
 
 	var response struct {
 		Data struct {
