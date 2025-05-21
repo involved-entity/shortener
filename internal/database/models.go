5c95a0a34318d3b414fbe5587e6e07d123491a06
Something Bad <karkadesnasvaem@gmail.com>
2025-05-21 19:34:32 +0300

    Add User relation in URL model, add BeforeCreate validation

--- internal/database/models.go
+++ internal/database/models.go
@@ -1,13 +1,18 @@
 package database
 
 import (
+	"errors"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type URL struct {
 	ID          uint      `gorm:"primaryKey" json:"id"`
 	OriginalURL string    `gorm:"not null" json:"originalURL"`
 	ShortCode   string    `gorm:"uniqueIndex;not null" json:"shortCode"`
+	UserID      *uint     `gorm:"index" json:"userId,omitempty"`
+	User        User      `gorm:"foreignKey:UserID;constraint:OnDelete:SET NULL" json:"user,omitempty"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
 }
@@ -27,4 +32,15 @@ type User struct {
 	Password   string    `gorm:"not null" json:"-"`
 	Email      string    `gorm:"uniqueIndex;not null" json:"email"`
 	IsVerified bool      `gorm:"default:false" json:"-"`
+	Urls       []URL     `gorm:"foreignKey:UserID;references:ID" json:"urls,omitempty"`
+}
+
+func (u URL) BeforeCreate(tx *gorm.DB) error {
+	if u.OriginalURL == "" {
+		return errors.New("originalURL cant be empty string")
+	}
+	if u.ShortCode == "" {
+		return errors.New("shortCode cant be empty string")
+	}
+	return nil
 }
