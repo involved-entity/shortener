a6c0abfd98c7ef4ccb4673aa254157f8ce64c53b
Something Bad <karkadesnasvaem@gmail.com>
2025-05-21 19:34:31 +0300

    Add GetURL service to get original url from database

--- internal/database/services.go
+++ internal/database/services.go
@@ -13,3 +13,12 @@ func SaveURL(db *gorm.DB, originalURL string, shortCode string) {
 	}
 	log.Println(url.ID)
 }
+
+func GetURL(db *gorm.DB, shortCode string) (string, error) {
+	var url URL
+	if err := db.Where("short_code = ?", shortCode).First(&url).Error; err != nil {
+		log.Println("Error when get a url", shortCode)
+		return "", err
+	}
+	return url.OriginalURL, nil
+}
