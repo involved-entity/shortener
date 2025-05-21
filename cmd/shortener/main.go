c32a87fd6f275e9ac4ff0c77c0e5eec0851cc86e
Something Bad <karkadesnasvaem@gmail.com>
2025-05-21 19:34:31 +0300

    Move logger setup to another file

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"log/slog"
-	"os"
 	"shortener/internal"
 	"shortener/internal/database"
 )
 
-const (
-	envLocal = "local"
-	envProd  = "prod"
-)
-
 func main() {
 	config := internal.MustLoad()
-	log := setupLogger(config.Env)
+	log := internal.SetupLogger(config.Env)
+
 	log.Info("Starting shortener service", "env", config.Env)
 	log.Info("HTTP server address", "address", config.HTTPServer.Address)
+
 	error := database.Init(config.DSN)
 	connection := database.GetDB()
+
 	log.Info("Database connection", "connection", connection, "error", error)
-}
-
-func setupLogger(env string) *slog.Logger {
-	var log *slog.Logger
-
-	switch env {
-	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-	}
-
-	return log
 }
---
+++ internal/logger.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
