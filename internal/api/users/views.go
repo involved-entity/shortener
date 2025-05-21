97e4065bb575270b49d0b94890346183afcae05f
Something Bad <karkadesnasvaem@gmail.com>
2025-05-21 19:34:33 +0300

    Add swagger docs for users endpoints

--- internal/api/users/views.go
+++ internal/api/users/views.go
@@ -8,6 +8,8 @@ import (
 	"shortener/internal/database"
 	"time"
 
+	_ "shortener/docs"
+
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/crypto/bcrypt"
@@ -58,6 +60,15 @@ type UpdateAccountDTO struct {
 	Email string `json:"email" validate:"required,email"`
 }
 
+// @Summary Регистрация пользователя
+// @Description Создает нового пользователя
+// @Accept json
+// @Produce json
+// @Param user body users.UserDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешная регистрация"
+// @Failure 400 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/register [post]
 func Register(c echo.Context) error {
 	dto := UserDTO{}
 	if err := api.DecodeRequest(c, &dto); err != nil {
@@ -82,6 +93,15 @@ func Register(c echo.Context) error {
 	return c.JSON(http.StatusOK, api.Response{Msg: "success", Data: user})
 }
 
+// @Summary Авторизация пользователя
+// @Description Авторизует пользователя
+// @Accept json
+// @Produce json
+// @Param user body users.UserLoginDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешная авторизация"
+// @Failure 401 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/login [post]
 func Login(c echo.Context) error {
 	dto := UserLoginDTO{}
 	if err := api.DecodeRequest(c, &dto); err != nil {
@@ -117,6 +137,15 @@ func Login(c echo.Context) error {
 	return c.JSON(http.StatusOK, api.Response{Msg: "success", Data: TokenDTO{Token: tokenString}})
 }
 
+// @Summary Регенерация кода подтверждения
+// @Description Регенерирует код подтверждения для пользователя
+// @Accept json
+// @Produce json
+// @Param user body users.RegenerateCodeDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешная регенерация"
+// @Failure 400 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/regenerate-code [post]
 func RegenerateCode(c echo.Context) error {
 	dto := RegenerateCodeDTO{}
 	if err := api.DecodeRequest(c, &dto); err != nil {
@@ -128,6 +157,15 @@ func RegenerateCode(c echo.Context) error {
 	return c.JSON(http.StatusOK, api.Response{Msg: "success"})
 }
 
+// @Summary Подтверждение почты
+// @Description Подтверждает почту пользователя
+// @Accept json
+// @Produce json
+// @Param user body users.VerificationDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешное подтверждение"
+// @Failure 400 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/verification [post]
 func ActivateAccount(c echo.Context) error {
 	dto := VerificationDTO{}
 	if err := api.DecodeRequest(c, &dto); err != nil {
@@ -147,6 +185,15 @@ func ActivateAccount(c echo.Context) error {
 	return c.JSON(http.StatusAccepted, api.Response{Msg: "success"})
 }
 
+// @Summary Сброс пароля
+// @Description Сбрасывает пароль пользователя
+// @Accept json
+// @Produce json
+// @Param user body users.ResetPasswordDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешный сброс пароля"
+// @Failure 400 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/reset-password [post]
 func ResetPassword(c echo.Context) error {
 	dto := ResetPasswordDTO{}
 	if err := api.DecodeRequest(c, &dto); err != nil {
@@ -166,6 +213,15 @@ func ResetPassword(c echo.Context) error {
 	return c.JSON(http.StatusAccepted, api.Response{Msg: "success"})
 }
 
+// @Summary Подтверждение сброса пароля
+// @Description Подтверждает сброс пароля пользователя
+// @Accept json
+// @Produce json
+// @Param user body users.ResetPasswordConfirmDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешное подтверждение сброса пароля"
+// @Failure 400 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/reset-password-confirm [post]
 func ResetPasswordConfirm(c echo.Context) error {
 	dto := ResetPasswordConfirmDTO{}
 	if err := api.DecodeRequest(c, &dto); err != nil {
@@ -190,6 +246,14 @@ func ResetPasswordConfirm(c echo.Context) error {
 	return c.JSON(http.StatusOK, api.Response{Msg: "success"})
 }
 
+// @Summary Получение информации о текущем пользователе
+// @Description Возвращает информацию о текущем пользователе
+// @Accept json
+// @Produce json
+// @Success 200 {object} api.Response "Успешный запрос"
+// @Failure 401 {object} api.Response "Неавторизованный запрос"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/account [get]
 func GetMe(c echo.Context) error {
 	userID := urls.GetUserID(c)
 	r := Repository{db: database.GetDB()}
@@ -200,6 +264,15 @@ func GetMe(c echo.Context) error {
 	return c.JSON(http.StatusOK, api.Response{Msg: "success", Data: user})
 }
 
+// @Summary Обновление аккаунта
+// @Description Обновляет информацию о пользователе
+// @Accept json
+// @Produce json
+// @Param user body users.UpdateAccountDTO true "Пользователь"
+// @Success 200 {object} api.Response "Успешное обновление"
+// @Failure 400 {object} api.Response "Некорректные данные"
+// @Failure 500 {object} api.Response "Внутренняя ошибка сервера"
+// @Router /api/account [patch]
 func UpdateAccount(c echo.Context) error {
 	userID := urls.GetUserID(c)
 	dto := UpdateAccountDTO{}
