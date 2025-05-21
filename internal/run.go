6b210d8091e25d3fbd618bc9d9e6c08d825fc599
Something Bad <karkadesnasvaem@gmail.com>
2025-05-21 19:34:32 +0300

    Move run logic to sep file

---
+++ internal/run.go
@@ -0,0 +1,38 @@
+package internal
+
+import (
+	"shortener/internal/api"
+	"shortener/internal/api/urls"
+	"shortener/internal/api/users"
+	"shortener/internal/database"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Run(config Config) {
+	log := SetupLogger(config.Env)
+
+	log.Info("Starting shortener service", "env", config.Env)
+
+	database.Init(config.DSN)
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.POST("/api/register", users.Register)
+	e.POST("/api/login", users.Login(config.TTL, config.SECRET))
+
+	e.GET("/_/:shortCode", urls.GetURL)
+
+	authProtected := e.Group("")
+	authProtected.Use(echojwt.WithConfig(echojwt.Config{SigningKey: []byte(config.SECRET), Skipper: api.OptionalJWT(
+		[]string{"/api/urls"},
+	)}))
+
+	authProtected.POST("/api/urls", urls.SaveURL)
+	authProtected.DELETE("/api/urls/:shortCode", urls.DeleteURL)
+
+	e.Start(config.Address)
+}
